d7e44a42dea73e7bd6ae6cbb399387ae00a6067c
Fitsum <63597726+fitsumtsehay@users.noreply.github.com>
2022-12-05 15:58:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Index():  # put application's code here
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM propertylist")
+    data = cur.fetchall()
+    cur.close()
+
     return render_template('index2.html', propertylist=data )
 
 if __name__ == '__main__':
