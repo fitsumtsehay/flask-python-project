80071f1881e64905d0787ce32851f25646d9b01b
Fitsum <63597726+fitsumtsehay@users.noreply.github.com>
2022-12-06 00:29:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
-def main():
-    return redirect(url_for('index2.html'))
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
