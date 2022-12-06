36de798b228ce41fbd5b5bfb4d63dc50cd604170
Fitsum <63597726+fitsumtsehay@users.noreply.github.com>
2022-12-05 21:02:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():  # put application's code here
-    return render_template('index2.html')
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
-if __name__ == '__main__':
+if __name__ == '__init__':
     app.run()
